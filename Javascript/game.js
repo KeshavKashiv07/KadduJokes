d908131335ac335747933a862657b0001b001696
Keshav <keshavkashiv9650@gmail.com>
2023-12-11 22:44:58 +0530

    done

--- Javascript/game.js
+++ Javascript/game.js
@@ -12,4 +12,51 @@ function gameDownload() {
     anchor_teg.click(); 
     //Remove the anchor element from the document (optional) 
     document.body.removeChild(anchor_teg);   
-  }S
+  }
+
+
+const the_animation_right = document.querySelectorAll('.animation_right')
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scroll-animation')
+        }
+            else {
+                entry.target.classList.remove('scroll-animation')
+            }
+        
+    })
+},
+   { threshold: 0.5
+   });
+   
+  for (let i = 0; i < the_animation_right.length; i++) {
+   const elements = the_animation_right[i];
+
+    observer.observe(elements);
+  }
+
+  
+  const the_animation_left = document.querySelectorAll('.animation_left')
+
+  const observer_left = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scroll-animation_left')
+        }
+            else {
+                entry.target.classList.remove('scroll-animation_left')
+            }
+        
+    })
+},
+   { threshold: 0.5
+   });
+
+  for (let i = 0; i < the_animation_left.length; i++) {
+    const elements = the_animation_left[i];
+ 
+     observer_left.observe(elements);
+   }
